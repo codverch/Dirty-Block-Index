d34550e8f9f8b1da46d563cc1ea8f93a9c031360
Deepanjali <mishradeepanjali25@gmail.com>
2023-01-05 22:02:17 +0000

    setDirtyBit()

--- src/mem/cache/rdbi/rdbi.cc
+++ src/mem/cache/rdbi/rdbi.cc
@@ -116,4 +116,83 @@ namespace gem5
             }
         }
     }
+
+    void
+    RDBI::setDirtyBit(PacketPtr pkt, unsigned int numSetBits, unsigned int numBlkBits,
+                      unsigned int numblkIndexBits)
+    {
+
+        // Get the complete address
+        Addr addr = pkt->getAddr();
+        // Shift right to remove the block offset bits
+        Addr temp = addr >> numBlkBits;
+        // Construct bitmask with 1's in the lower (z) bits from notes
+        int bitmask = (1 << numBlkIndexBits) - 1;
+        // AND the bitmask with the shifted address to get the block index
+        int blkIndex = temp & bitmask;
+
+        regAddr = getRegDBITag(pkt);
+
+        // Identify the entry
+        rDBIIndex = getRDBIEntryIndex(pkt, numSetBits, numBlkBits, numblkIndexBits);
+
+        // Get the inner vector of DBI entries at the specified index location
+        vector<RDBIEntry> &rDBIEntries = rDBIStore[rDBIIndex];
+
+        // Iterate through the inner vector of DBI entries using an iterator
+        for (vector<DBIEntry>::iterator i = rDBIEntries.begin(); i != rDBIEntries.end(); ++i)
+        {
+            RDBIEntry &entry = *i;
+            if (entry.regTag == regAddr)
+            {
+                // If the entry is valid, return the dirty bit
+                if (entry.validBit == 1)
+                {
+                    // Check the entry's dirty bit from the bitset
+                    entry.dirtyBits.set(blkIndex);
+                }
+            }
+
+            else
+            {
+                // If there wasn't a match for the regTag, iterate over the generated index and
+                // check if there is an invalid entry at the rDBIIndex
+                for (vector<DBIEntry>::iterator i = rDBIEntries.begin(); i != rDBIEntries.end(); ++i)
+                {
+                    RDBIEntry &entry = *i;
+                    if (entry.validBit == 0)
+                    {
+                        // If there is an invalid entry, set the valid bit to 1
+                        entry.validBit = 1;
+                        // Set the regTag to the current regAddr
+                        entry.regTag = regAddr;
+                        // Set the dirty bit at the block index
+                        entry.dirtyBits.set(blkIndex);
+                        // Break out of the loop
+                        break;
+                    }
+
+                    // If there wasn't an invalid entry, then we need to evict an entry and replace it, but at the same rDBIIndex
+                    else
+                    {
+                        // NEEDS TO DO WRITEBACK
+                        // Evict the entry at the rDBIIndex
+                        rDBIEntries.erase(i);
+                        // Create a new entry
+                        RDBIEntry newEntry;
+                        // Set the valid bit to 1
+                        newEntry.validBit = 1;
+                        // Set the regTag to the current regAddr
+                        newEntry.regTag = regAddr;
+                        // Set the dirty bit at the block index
+                        newEntry.dirtyBits.set(blkIndex);
+                        // Push the new entry into the rDBIEntries vector
+                        rDBIEntries.push_back(newEntry);
+                        // Break out of the loop
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
