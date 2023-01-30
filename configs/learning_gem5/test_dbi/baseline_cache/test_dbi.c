3952fc53a5bb753fe30f2d6989f46178d088127a
Deepanjali <mishradeepanjali25@gmail.com>
2023-01-30 06:55:28 +0000

    test_dbi.c

--- configs/learning_gem5/test_dbi/baseline_cache/test_dbi.c
+++ configs/learning_gem5/test_dbi/baseline_cache/test_dbi.c
@@ -2,15 +2,15 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <time.h>
+#include <typeinfo>
 
 int main(int argc, char *argv[])
 {
-    // An array on n elements
+    // An array of 4 million integer elements
     int *arr;
 
     // Command line arguments
-    int k_blocks, iter, opt;
-
+    int k_blocks = 1, iter = 1, opt;
     while ((opt = getopt(argc, argv, ":k:t")) != -1)
     {
         switch (opt)
@@ -21,30 +21,50 @@ int main(int argc, char *argv[])
                 k_blocks = atoi(optarg);
 
         case 't':
-            iter = atoi(argv[4]);
-            break;
-
-        default:
+            if (optarg != NULL)
+                iter = atoi(optarg);
             break;
         }
     }
 
     // Allocate memory for the array
     arr = (int *)malloc(4000000 * sizeof(int));
+    if (arr == NULL)
+    {
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
 
     // Every DRAM row contains 64 cache blocks, and each cache block is 64 bytes
     // Each cache block can store 16 integers
     // So, the number of integers that can be stored in a DRAM row is 1024
 
-    // Initialize the array with random values from 0 to 99
+    // Initialize the array with zeroes
     for (int i = 0; i < 4000000; i++)
-        arr[i] = rand() % 100;
-
-    // Choose a random DRAM row and write to k blocks in that row
-    int row = rand() % 4000;
-    int start = row * 1024;
-    int end = start + k_blocks * 16;
-    // Write to the array
-    for (int i = start; i < end; i++)
         arr[i] = 0;
+
+    // For t iterations, : choose a random region => write to k cache blocks within the region;
+
+    int i = 0;
+    while (i < iter)
+    {
+        // Write to one element each in random k cache blocks
+
+        for (int j = 1; j <= k_blocks; j++)
+        {
+            // Choose a random DRAM row
+            int row_id = rand() % 4000;
+
+            // Choose a random cache block within the DRAM row
+            int block_id = rand() % 64;
+
+            // Write to one element in the cache block
+            arr[row_id * 1024 + block_id * 16] = 1;
+        }
+
+        i++;
+    }
+
+    // Free the memory
+    free(arr);
 }
