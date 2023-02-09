9c1d42cbcbfc27c265c03c30001c61ce2c9f6f65
Deepanjali <mishradeepanjali25@gmail.com>
2023-02-09 11:58:58 +0000

    configuration for baseline-test

---
+++ configs/learning_gem5/test/three_level.py
@@ -0,0 +1,140 @@
+
+# import the m5 (gem5) library created when gem5 is built
+import m5
+import sys
+# import all of the SimObjects
+from m5.objects import *
+
+# Add the common scripts to our path
+m5.util.addToPath('../../')
+
+# import the caches which we made
+from caches import *
+
+# import the SimpleOpts module
+from common import SimpleOpts
+
+
+# get ISA for the default binary to run. This is mostly for simple testing
+isa = str(m5.defines.buildEnv['TARGET_ISA']).lower()
+
+# Default to running 'hello', use the compiled ISA to find the binary
+# grab the specific path to the binary
+thispath = os.path.dirname(os.path.realpath(__file__))
+default_binary = sys.argv[1]
+
+# Binary to execute
+SimpleOpts.add_option("binary", nargs='?', default=default_binary)
+
+# Finalize the arguments and grab the args so we can pass it on to our objects
+args = SimpleOpts.parse_args()
+
+# create the system we are going to simulate
+system = System()
+
+# Set the clock frequency of the system (and all of its children)
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '1GHz'
+system.clk_domain.voltage_domain = VoltageDomain()
+
+# Set up the system
+system.mem_mode = 'timing'               # Use timing accesses
+system.mem_ranges = [AddrRange('8192MB')] # Create an address range
+
+# Create a simple CPU
+system.cpu = TimingSimpleCPU()
+
+
+# Create an L1 instruction and data cache
+system.cpu.icache = L1ICache(args)
+system.cpu.dcache = L1DCache(args)
+
+# Connect the instruction and data caches to the CPU
+system.cpu.icache.connectCPU(system.cpu)
+system.cpu.dcache.connectCPU(system.cpu)
+
+# Create a memory bus, a coherent crossbar, in this case
+system.l2bus = L2XBar() # L2 bus
+system.l3bus = L2XBar()
+
+
+# Hook the CPU ports up to the l2bus
+system.cpu.icache.connectBus(system.l2bus)
+system.cpu.dcache.connectBus(system.l2bus)
+
+# Create an L2 cache and connect it to the l2bus
+system.l2cache = L2Cache(args)
+system.l2cache.connectCPUSideBus(system.l2bus)
+
+# Create an L3 cache and connect it to the l2 cache on the CPU side 
+system.l3cache = L3Cache(args)
+system.l3cache.connectCPUSideBus(system.l3bus)
+# Create a memory bus
+system.membus = SystemXBar()
+
+system.l2cache.connectMemSideBus(system.l3bus)
+
+# Connect the L3 cache to the membus 
+system.l3cache.connectMemSideBus(system.membus)
+
+# create the interrupt controller for the CPU
+system.cpu.createInterruptController()
+
+# For x86 only, make sure the interrupts are connected to the memory
+# Note: these are directly connected to the memory bus and are not cached
+if m5.defines.buildEnv['TARGET_ISA'] == "x86":
+    system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+    system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+    system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+
+# Connect the system up to the membus
+system.system_port = system.membus.cpu_side_ports
+
+# Create a DDR3 memory controller
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+# Change the number of DRAM ranks to 1
+system.mem_ctrl.dram.ranks_per_channel = 1
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+system.workload = SEWorkload.init_compatible(args.binary)
+
+# Create a process for a simple "Addition of arrays" application
+process = Process()
+# Set the command
+
+# Set the cpu to use the process as its workload and create thread contexts
+system.cpu.workload = process
+system.cpu.createThreads()
+
+import argparse
+
+parser = argparse.ArgumentParser(description='A simple system with 3-level cache.')
+parser.add_argument("binary", default="", nargs="?", type=str,
+                    help="Path to the binary to execute.")
+parser.add_argument("--l1i_size",
+                    help=f"L1 instruction cache size. Default: 16kB.")
+parser.add_argument("--l1d_size",
+                    help="L1 data cache size. Default: Default: 64kB.")
+parser.add_argument("--l2_size",
+                    help="L2 cache size. Default: 256kB.")
+parser.add_argument("--l3_size",
+                    help="L2 cache size. Default: 4096kB.")
+parser.add_argument("-n", "--nums", type=int, help="number of elements in each array", default="1")
+parser.add_argument("-k", "--blocks", type=int, help="number of random blocks to choose", default="4")
+parser.add_argument("-t", "--iterations", type=int, help="number of iterations", default="1")
+
+options = parser.parse_args()
+
+# cmd is a list which begins with the executable (like argv)
+process.cmd = [args.binary, '-n', options.nums, '-k', options.blocks, '-t', options.iterations]
+
+# set up the root SimObject and start the simulation
+root = Root(full_system = False, system = system)
+# instantiate all of the objects we've created above
+m5.instantiate()
+
+print("Beginning simulation!")
+exit_event = m5.simulate()
+print('Exiting @ tick %i because %s' % (m5.curTick(), exit_event.getCause()))
---
+++ test-benchmark/test.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    int *arr;
+
+    // Command line arguments
+    int k_blocks = 1, iter = 1, num_ele, opt;
+
+    while ((opt = getopt(argc, argv, ":n:k:t:")) != -1)
+    {
+        switch (opt)
+        {
+        // Use atoi(optarg) instead of atoi(argv[2])
+        case 'n':
+            if (optarg != NULL)
+                num_ele = atoi(optarg);
+            break;
+
+        case 'k':
+            if (optarg != NULL)
+                k_blocks = atoi(optarg);
+            break;
+
+        case 't':
+            if (optarg != NULL)
+                iter = atoi(optarg);
+            break;
+        }
+    }
+
+    // Print the values of the command line arguments
+    printf("Number of elements: %d, Number of cache blocks: %d, Number of iterations: %d\n", num_ele, k_blocks, iter);
+
+    // Allocate memory for the array
+    arr = (int *)malloc(num_ele * sizeof(int));
+
+    // Initialize the array with zeroes
+    for (int i = 0; i < num_ele; i++)
+        arr[i] = 0;
+
+    // For t iterations, : choose a random region => write to k cache blocks within the region;
+    // Write to random k_blocks of the same DRAM row
+
+    srand(33);
+
+    int i = 0;
+    while (i < iter)
+    {
+        // Choose a random DRAM row
+        int row_id = rand() % (num_ele / 1024);
+
+        for (int j = 1; j <= k_blocks; j++)
+        {
+            // Choose a random cache block within the DRAM row
+            int block_id = rand() % 64;
+
+            int d = row_id * 1024 + block_id * 16;
+
+            // Write to the first element of the cache block
+            arr[d] = 1;
+        }
+
+        i++;
+    }
+
+    // Compute the sum of all the elements in the array
+    int sum = 0;
+    for (int i = 0; i < num_ele; i++)
+        sum += arr[i];
+
+    // Print the sum
+    printf("Sum: %d \n", sum);
+
+    // Free the memory
+    free(arr);
+}
