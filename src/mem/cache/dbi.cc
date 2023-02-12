076daf3f5922c3aad598a7e70f1b5e12b3caff55
Deepanjali <mishradeepanjali25@gmail.com>
2023-02-12 19:04:03 +0000

    dbi.cc

--- src/mem/cache/dbi.cc
+++ src/mem/cache/dbi.cc
@@ -598,4 +598,66 @@ namespace gem5
         return blk;
     }
 
-} // namespace gem5
+    DBICache::DBICmdStats::DBICmdStats(DBICache &c, const std::string &name)
+        : statistics::Group(&c, name.c_str()), dbiCache(c),
+
+    {
+    }
+
+    void
+    DBICache::DBICmdStats::regStatsFromParentDBI()
+    {
+        using namespace statistics;
+
+        statistics::Group::regStats();
+        System *system = dbiCache.system;
+        const auto max_requestors = system->maxRequestors();
+    }
+
+    DBICache::DBICacheStats::DBICacheStats(DBICache &c)
+        : statistics::Group(&c), dbiCache(c),
+
+          ADD_STAT(agrWritebacks, statistics::units::Count::get(),
+                   "Number of writebacks generated due to aggressive writeback"),
+          cmd(MemCmd::NUM_MEM_CMDS)
+
+    {
+        for (int idx = 0; idx < MemCmd::NUM_MEM_CMDS; ++idx)
+        {
+            cmd[idx].reset(new DBICmdStats(c, MemCmd(idx).toString()));
+        }
+    }
+
+    void
+    DBICache::DBICacheStats::regStats()
+    {
+        using namespace statistics;
+
+        statistics::Group::regStats();
+
+        System *system = dbiCache.system;
+        const auto max_requestors = system->maxRequestors();
+        for (auto &cs : cmd)
+            cs->regStatsFromParentDBI();
+
+#define SUM_DEMAND(s)                                           \
+    (cmd[MemCmd::ReadReq]->s + cmd[MemCmd::WriteReq]->s +       \
+     cmd[MemCmd::WriteLineReq]->s + cmd[MemCmd::ReadExReq]->s + \
+     cmd[MemCmd::ReadCleanReq]->s + cmd[MemCmd::ReadSharedReq]->s)
+
+// should writebacks be included here?  prior code was inconsistent...
+#define SUM_NON_DEMAND(s)                                    \
+    (cmd[MemCmd::SoftPFReq]->s + cmd[MemCmd::HardPFReq]->s + \
+     cmd[MemCmd::SoftPFExReq]->s)
+    }
+
+    agrWritebacksGenerated
+        .init(max_requestors)
+        .flags(total | nozero | nonan);
+    for (int i = 0; i < max_requestors; i++)
+    {
+        agrWritebacksGenerated.subname(i, system->getRequestorName(i));
+    }
+}
+
+// namespace gem5
